5d0171c211a6e2017a2608b58ca9f999b1494fdd
deepesh-sr <rathoredeepeshsingh@gmail.com>
2026-02-10 01:23:08 +0530

    Add hero section sliders

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import './App.css'
 import Navbar from './components/Navbar'
+import HeroSlider from './components/HeroSlider'
 
 function App() {
   return (
     <>
       <Navbar />
-      <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-        <p className='text-5xl font-[--font-family-inter]'>
-          Deepesh this side.
-        </p>
-      </main>
+      <HeroSlider />
     </>
   )
 }
